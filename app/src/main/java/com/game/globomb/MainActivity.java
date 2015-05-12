20329261bd77d0c024044af59bd1b2efc6d704a0
denizsokmen <deniz.sokmen@ozu.edu.tr>
2015-05-12 11:41:49 +0300

    maps

--- app/src/main/java/com/game/globomb/MainActivity.java
+++ app/src/main/java/com/game/globomb/MainActivity.java
@@ -1,9 +1,13 @@
 package com.game.globomb;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.enterbutton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+
     }
 
 
